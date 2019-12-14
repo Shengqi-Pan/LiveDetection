4a5434f67668121c4963c585635d570c569b06c8
Shengqi-Pan <1417625301@qq.com>
2019-12-14 22:18:45 +0800

    references
    上传了一些语音识别的资料

---
+++ src/main.m
@@ -0,0 +1,18 @@
+clear;clc;close all;
+recObj = audiorecorder;
+disp('Start speaking.')
+recordblocking(recObj, 5);
+disp('End of Recording.');
+play(recObj);
+y = getaudiodata(recObj);
+plot(y);
+
+fc = 4000;
+fs = 8000;bits = 8;
+N = 2 * pi * 3.1 / (0.1 * pi);
+wc = 2 * pi * fc / fs;
+N = N + mod(N, 2);
+Window = hanning(N + 1);
+b = fir1(N, 0.48, 'high', Window);
+y_high = filter(b, 1, y);
+sound(y_high, fs, bits);
