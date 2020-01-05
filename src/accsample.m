c8cacf4120609a8cfdc67a93e87d734c0c808ee2
zju_vc <btk@zju.edu.cn>
2020-01-05 20:32:36 +0800

    解决了串口bug

--- src/accsample.m
+++ src/accsample.m
@@ -1,5 +1,16 @@
 %用于设定定时器进行采样
+R = audiorecorder(8000, 16 ,1);
+record(R);
 % s = mySerial;
-% s = s.init('com5');
-t=timer('TimerFcn','s = s.read();','Period',0.001,'ExecutionMode','fixedSpacing','TasksToExecute',1000);
-start(t)
+% s = s.init('com4');
+% t=timer('TimerFcn','s = s.read();','Period',0.001,'ExecutionMode','fixedSpacing','TasksToExecute',5000);
+% start(t)
+% fread(s.s, s.s.BytesAvailable);
+flushinput(s.s)
+for i = 1 : 5000
+    s = s.read();
+    pause(0.0009)
+end
+stop(R)
+plot(s.acc_signal);
+disp('a')
--- src/record.m
+++ src/record.m
@@ -1,19 +1,25 @@
 %½øÐÐÂ¼Òô
-clear;clc;close all;
-recObj = audiorecorder;
-disp('Start speaking.')
-recordblocking(recObj, 5);
-disp('End of Recording.');
-play(recObj);
-y = getaudiodata(recObj);
-plot(y);
+% clear;clc;close all;
+% recObj = audiorecorder;
+% disp('Start speaking.')
+% recordblocking(recObj, 5);
+% disp('End of Recording.');
+% play(recObj);
+% y = getaudiodata(recObj);
+% plot(y);
 
-fc = 4000;
-fs = 8000;bits = 8;
-N = 2 * pi * 3.1 / (0.1 * pi);
-wc = 2 * pi * fc / fs;
-N = N + mod(N, 2);
-Window = hanning(N + 1);
-b = fir1(N, 0.48, 'high', Window);
-y_high = filter(b, 1, y);
-sound(y_high, fs, bits);
+% fc = 4000;
+% fs = 8000;bits = 8;
+% N = 2 * pi * 3.1 / (0.1 * pi);
+% wc = 2 * pi * fc / fs;
+% N = N + mod(N, 2);
+% Window = hanning(N + 1);
+% b = fir1(N, 0.48, 'high', Window);
+% y_high = filter(b, 1, y);
+% sound(y_high, fs, bits);
+
+%%%%%%%%%%%%%%%%%%%%
+ R = audiorecorder(8000, 16 ,1);
+ record(R);
+ disp('1');
+ 
