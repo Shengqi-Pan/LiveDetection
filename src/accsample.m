1b23bf1073a6a84b0a65b4b5c19083502b60dc7a
Shengqi-Pan <1417625301@qq.com>
2020-01-05 22:38:11 +0800

    完成检测
    完成了检测的代码，更新了一些数据

--- src/accsample.m
+++ src/accsample.m
@@ -1,16 +1,27 @@
 %用于设定定时器进行采样
 R = audiorecorder(8000, 16 ,1);
 record(R);
-% s = mySerial;
-% s = s.init('com4');
+s = mySerial;
+s = s.init('com5');
 % t=timer('TimerFcn','s = s.read();','Period',0.001,'ExecutionMode','fixedSpacing','TasksToExecute',5000);
 % start(t)
 % fread(s.s, s.s.BytesAvailable);
-flushinput(s.s)
+% flushinput(s.s)
 for i = 1 : 5000
     s = s.read();
-    pause(0.0009)
+    pause(0.0008)
 end
 stop(R)
+
+if var(s.acc_signal) < 20
+    [y, fs] = audioread('..\data\无权限.mp3');
+    sound(y,fs);           % 回放语音信号
+else
+    play(R);
+end
+
 plot(s.acc_signal);
-disp('a')
+
+figure();
+y = getaudiodata(R);
+plot(y)
